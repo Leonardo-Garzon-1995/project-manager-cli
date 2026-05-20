53d37190a2b9dc12d2093f29ef8fc954055764a9
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-05-20 04:15:45 +0200

    chore(tasks): clean up unused methods, keep minimum logic for class

--- src/projects/task.js
+++ src/projects/task.js
@@ -1,7 +1,5 @@
-import * as readline from 'node:readline/promises'
-import { stdin as input, stdout as output } from 'node:process'
+import crypto from 'crypto'
 
-const rl = readline.createInterface({ input, output, terminal: false })
 export default class Task {
     constructor(title) {
         this.id = `tsk-${crypto.randomUUID()}`
@@ -16,27 +14,6 @@ export default class Task {
     markAsCompleted() {
         this.completed = true
     }
-
-    async updateTitle() {
-        const title = await rl.question("Enter task title: ")
-        if (!title || typeof title !== 'string') {
-            console.log("A tile is required and must be a string")
-            title = await rl.question("Enter task title: ")
-        }
-        this.title = title.trim()
-    }
-
-    async updateDueDate() {
-        const dueDate = await rl.question("Enter due date (YYYY-MM-DD): ")
-        this.dueDate = new Date(dueDate).toLocaleDateString()
-        if (!dueDate || this.dueDate !== 'Invalid Date') {
-            console.log("A due date is required and must be a string")
-            dueDate = await rl.question("Enter due date (YYYY-MM-DD): ")
-        }
-    }
-
 }
-
-rl.close()
 
 
