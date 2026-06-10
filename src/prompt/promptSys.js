59a97531ccadccfd62f46011d8998053bdc9e452
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-06-10 04:44:11 +0200

    feat: centraliza prompt system to reduce clutter and duplication of code

---
+++ src/prompt/promptSys.js
@@ -0,0 +1,26 @@
+import readline from 'node:readline/promises'
+import { stdin as input, stdout as output} from 'node:process'
+
+import * as logger from '../helpers/logger.js'
+
+export default class Prompt {
+
+    static async ask(message) {
+        const rl = readline.createInterface({
+            input,
+            output,
+            terminal: false
+        })
+
+        try {
+            logger.info(`Prompt initialized`)
+            return await rl.question(message)
+        } catch (error) {
+            logger.error(error.stack)
+        } finally {
+            rl.close()
+            logger.info('Prompt closed without issues')
+        }
+
+    }
+}
