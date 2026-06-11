e1d7860d106dde6c983bfd8acda97c9d67197420
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-06-11 08:47:02 +0200

    fix(task.js): the task class uses a internal helper to generate a timestamp for the date system

--- src/projects/task.js
+++ src/projects/task.js
@@ -1,4 +1,5 @@
 import Id from '../helpers/id_system.js'
+import { generateTimestamp } from '../helpers/dates.js'
 
 export default class Task {
     constructor(title) {
@@ -6,7 +7,7 @@ export default class Task {
         this.title = title.trim()
         this.proId = null
         this.proKeyword = null
-        this.createdAt = new Date().toLocaleDateString()
+        this.createdAt = generateTimestamp()
         this.completed = false
         this.dueDate = null
     }
