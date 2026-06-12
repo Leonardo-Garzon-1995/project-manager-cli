d8f505acfbfd990ac4c74171c2d0d7f84fac0229
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-06-12 08:57:32 +0200

    refactor(bin/main): refactor entry point with a simplified command handling

--- bin/main.js
+++ bin/main.js
@@ -4,110 +4,54 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import displayHelp  from '../src/helpers/displayHelp.js'
 import { displayDeafultHeader } from '../src/helpers/defaultUI.js'
+import projectCommand from '../src/commands/projectCommand.js'
+import taskCommand from '../src/commands/taskCommand.js'
+import NoteCommand from '../src/commands/noteCommand.js'
+
+import * as logger from '../src/helpers/logger.js'
 
 import ProjectsManager from '../src/projects/projects-manager.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const FILE = path.join(__dirname, '../data/projects.json')
+const FILE = path.join(__dirname, '..', 'data', 'projects.json')
 
 const [, , command, ...args] = process.argv
 
 const manager = new ProjectsManager(FILE)
 
-function main(cmd) {    
-    if (!cmd) {
-        displayDeafultHeader()
-        manager.displayDailyTasks()
-        return
-    }
+function main(cmd) {
+    try {
+        if (!cmd) {
+            displayDeafultHeader()
+            manager.displayDailyTasks()
+            return
+        }
         
-    switch(cmd) {
-        case "add-project":
-        case "-ap":
-            manager.addProject(FILE)
-            break;
-        case "list-projects":
-        case "-lp":
-            manager.listProjects()
-            break;
-        case "delete-project":
-        case "-dp":
-            manager.deleteProjectByIndex(FILE, args[0])
-            break;
-        case "view-project":
-        case "-vp":
-            manager.viewProjectByIndex(args[0])
-            break;
-        case "toggle-importance":
-        case "-ti":
-            manager.toggleProjectImportance(FILE,args[0])
-            break;
-        case "clear-projects":
-            manager.clearAllProjects(FILE)
-            break;
-        case "add-task":
-        case "-at":
-            manager.addTaskToProjectByIndex(FILE, args[0])
-            break;
-        case "list-tasks":
-        case "-lt":
-            manager.listTasksByProjectIndex(args[0])
-            break;
-        case "list-completed-tasks":
-        case "-lct":
-            manager.listCompletedTasksByIndex(args[0])
-            break;
-        case "list-pending-tasks":
-        case "-lpt":
-            manager.listPendingTasksByIndex(args[0])
-            break;
-        case 'tasks-bydate':
-        case '-tbd':
-            manager.displayTasksByDate(args[0])
-            break
-        case "task-completed":
-        case "-tc":
-            manager.markTaskAsCompleted(FILE, args[0], args[1])
-            break;
-        case "clear-tasks":
-            manager.clearTasksByProjectIndex(FILE, args[0])
-            break;
-        case "view-task":
-        case "-vt":
-            manager.viewTaskByIndex(args[0], args[1])
-            break;
-        case "delete-task":
-        case "-dt":
-            manager.deleteTask(FILE, args[0], args[1])
-            break;
-        case "help":
-        case "-h":
-            displayHelp()
-            break;
-        case 'add-note':
-            manager.createNoteToProject(FILE, args[0])
-            break;
-        case 'read-note':
-            manager.readNoteFromProject(args[0], args[1])
-            break
-        case 'append-note':
-            manager.appendToNoteByProject(args[0], args[1])
-            break;
-        case 'delete-note':
-            manager.deleteNotefromProject(FILE, args[0], args[1])
-            break;
-        case 'clear-notes':
-            manager.clearAllNotesFromProject(FILE, args[0])
-            break;
-        case 'list-notes':
-            manager.listNotesByProject(args[0])
-            break
-        default:
-            console.log(`   Invalid command: <${cmd}>\n`)
-            displayHelp()
-            break;
+        switch(cmd) {
+            case 'project':
+                projectCommand(args, manager, FILE)
+                break;
+            case 'task':
+                taskCommand(args, manager, FILE)
+                break
+            case 'note':
+                NoteCommand(args, manager, FILE)
+                break;
+            case "help":
+            case "-h":
+                displayHelp()
+                break;
+            default:
+                console.log(`   Invalid command: <${cmd}>\n`)
+                displayHelp()
+                break;
+        }
+    } catch (error) {
+        logger.error(error.stack)
+        console.error(error.message)
     }
+    
 }
 
 main(command)
