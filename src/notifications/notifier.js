7078c61d149c4778442b0b6b1b5bfd12d6b8dbdc
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-07-28 05:40:31 -0600

    refactor(notifications/notifier): implements the new NotificationError for error handling and imports the getResendClient function from config/.

--- src/notifications/notifier.js
+++ src/notifications/notifier.js
@@ -1,8 +1,8 @@
-import 'dotenv/config'
-import { Resend } from 'resend'
+// import 'dotenv/config'
+import { getResendClient } from '../config/resend.js' 
 import * as logger from '../helpers/logger.js'
+import { NotificationError } from '../errors.js'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
 
 /**
  * 
@@ -13,6 +13,9 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 export default class Notify {
     static async directEmail(subjectText, htmlMessage) {
         try {
+
+            const resend = getResendClient()
+
             const { data, error } = await resend.emails.send({
                 from: 'onboarding@resend.dev',
                 to: 'lgarzonlc@gmail.com',
@@ -21,11 +24,10 @@ export default class Notify {
                 html: htmlMessage || 'No body'
             })
             if (error) {
-                throw new Error(error.message)
+                throw new NotificationError(error.message)
             }
 
             logger.info(`Email has been succesfully sent. Email id: ${data.id}`)
-
             
         } catch(err) {
             logger.error(err.stack)
@@ -34,6 +36,9 @@ export default class Notify {
 
     static async scheduledEmail(subjectText, htmlMessage, schedule) {
         try {
+
+            const resend = getResendClient()
+            
             const { data, error } = await resend.emails.send({
                 from: 'onboarding@resend.dev',
                 to: 'lgarzonlc@gmail.com',
@@ -43,15 +48,13 @@ export default class Notify {
                 scheduledAt: schedule || 'in 5 min'
             })
             if (error) {
-                throw new Error(error.message)
+                throw new NotificationError(error.message)
             }
 
             logger.info(`Task email has been succesfully sent. Email id: ${data.id}`)
-
             
         } catch(err) {
             logger.error(err.stack)
         }
     }
-
 }
